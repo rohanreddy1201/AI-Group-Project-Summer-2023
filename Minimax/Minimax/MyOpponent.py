14b5a67c3dd12faae0ec782dcba71c611bdf4919
Rohan Reddy <55873218+rohanreddy1201@users.noreply.github.com>
2024-03-27 16:02:33 -0700

    Delete Minimax/Minimax directory

--- Minimax/Minimax/MyOpponent.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-
-class MyOpponent:
-    def get_move(self, grid):
-        c = grid.get_empty_cells()
-        if c:
-            return c[randint(0, len(c) - 1)]
-        else:
-            return None
